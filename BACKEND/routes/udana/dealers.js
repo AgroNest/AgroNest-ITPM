3295626fcc6de219b5992124f607cf8ede7e2b11
VeenathT <veenaththamuditha@gmail.com>
2025-03-21 13:07:44 +0530

    udana code convert to - bcryptjs

--- BACKEND/routes/udana/dealers.js
+++ BACKEND/routes/udana/dealers.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 let Dealer = require('../../models/udana/dealer');
 let Fertilizer = require('../../models/udana/fertilizer');
