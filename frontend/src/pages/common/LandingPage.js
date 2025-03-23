e2b9ff5b08d10f522ac211fc460f10b060029c66
VeenathT <veenaththamuditha@gmail.com>
2025-03-23 11:50:32 +0530

    correct common pages

--- frontend/src/pages/common/LandingPage.js
+++ frontend/src/pages/common/LandingPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ArticleList from '../Nilupul/ArticleList';
+import ArticleList from '../kande/ArticleList';
 import one from '../../images/common/slider/1.png';
 import two from '../../images/common/slider/2.png';
-import three from '../../images/Sudarshan/fertilizer images/tsp.png';
+import three from '../../images/thamuditha/fertilizer images/tsp.png';
 
 import SlideShow from '../../Component/common/Slideshow';
 
@@ -14,9 +14,9 @@ const images = [
 
 const DealerSignUp = () => {
   const slideshowImages = [
-    require('../../images/Rahul/1.png'),
-    require('../../images/Rahul/2.png'),
-    require('../../images/Rahul/3.png')
+    require('../../images/thamuditha/1.png'),
+    require('../../images/thamuditha/2.png'),
+    require('../../images/thamuditha/3.png')
   ];
 
   return (
